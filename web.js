c656394b807e43609cad6fd0e09dadf08e6e4c6f
Ubuntu <ubuntu@ip-10-236-156-136.ec2.internal>
2013-07-19 21:16:49 +0000

    Trivial - Fixes

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(new Buffer(fs.readFileSync('index.html')).toString());
 });
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
